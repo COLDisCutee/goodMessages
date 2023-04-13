42b8d62af6bd97108813a1a8c6d96c6b210a8cfc
COLD <coldredtea@gmail.com>
2023-04-13 20:41:35 +0900

    배포3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const url = new URL('https://api.qwer.pw/request/helpful_text');
+let soundplayed = false
 const params = new 
 URLSearchParams({apikey: 'guest'});
 let answer
@@ -14,13 +15,22 @@ function answerGet(data){
     answer = (data[1].respond).split("-")[0]
     by = (data[1].respond).split("-")[1]
   }
+  if (soundplayed == false){
+    let audio = new Audio('audio/Aakash Gandhi - One Step Closer (Romantic).mp3');
+    audio.volume = 0.25;
+audio.play();
+    soundplayed = true;
+  }
+    document.querySelector(".goodMessage, .by").style.transition = "opacity 1s"
   document.querySelector(".goodMessage").innerText = answer
    document.querySelector(".goodMessage").style.opacity = "100%"
    document.querySelector(".by").innerText = "- "+by+" -"
   
      document.querySelector(".by").style.opacity = "100%"
 }
-fetch(url)
+function newMessage(){
+ fetch(url)
   .then(response => response.json())
-  .then(answerGet)
-  .catch(error => console.error(error));
+  .then(answerGet); 
+}
+document.querySelector(".startButton").addEventListener('click', newMessage)
